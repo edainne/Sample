5dcc038fedb5874195566b4ba2c275956a9c20ea
edainne <edaine@klab.com>
2012-10-30 16:55:00 +0800

    Initial commit

---
+++ Sample/Sample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sample
+//
+//  Created by Edainne Ladys S. Silva on 10/30/12.
+//  Copyright (c) 2012 Edainne Ladys S. Silva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sample/Sample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sample
+//
+//  Created by Edainne Ladys S. Silva on 10/30/12.
+//  Copyright (c) 2012 Edainne Ladys S. Silva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
