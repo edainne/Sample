4b9170f68eed0764ab27c13d9bfce6771c7479d5
edainne <edaine@klab.com>
2012-10-30 16:56:55 +0800

    Added view controllers

---
+++ Sample/SampleViewController.h
@@ -0,0 +1,13 @@
+//
+//  SampleViewController.h
+//  Sample
+//
+//  Created by Edainne Ladys S. Silva on 10/30/12.
+//  Copyright (c) 2012 Edainne Ladys S. Silva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SampleViewController : UIViewController
+
+@end
---
+++ Sample/SampleViewController.m
@@ -0,0 +1,44 @@
+//
+//  SampleViewController.m
+//  Sample
+//
+//  Created by Edainne Ladys S. Silva on 10/30/12.
+//  Copyright (c) 2012 Edainne Ladys S. Silva. All rights reserved.
+//
+
+#import "SampleViewController.h"
+
+@interface SampleViewController ()
+
+@end
+
+@implementation SampleViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
